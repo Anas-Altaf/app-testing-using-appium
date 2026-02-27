6f74ba2c8d6108a5b174977dcf343c0de5ad9e4b
Anas-Altaf <anasaltaf35@gmail.com>
2026-02-27 08:23:07 +0500

    Fix previous setup issues in Driver  Run the first Login Test, Ensure everythin is setup and ready Add Quick Alert Apk for Testing

--- utils/driver.py
+++ utils/driver.py
@@ -8,11 +8,14 @@ def get_driver():
 
     options = UiAutomator2Options()
     options.platform_name = "Android"
-    options.device_name = "RF8M50CQK5H"
+    options.device_name = "emulator-5554" # Change this to your device name or emulator ID
     options.automation_name = "UiAutomator2"
     options.app = apk_path   # Appium will auto-install this APK
     # setting timeout to 30 seconds, uiautomator2ServerInstallTimeout
-    options.uiautomator2_server_install_timeout = 30000
+    options.uiautomator2_server_install_timeout = 60000
+
+    # Auto grant permissions
+    options.auto_grant_permissions = True
     
 
     driver = webdriver.Remote("http://localhost:4723", options=options)
